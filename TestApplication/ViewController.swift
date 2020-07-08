64ec002e297f447cf6cd0e3f969350a7458ffeec
Muhammad <abdurazakov96@mail.ru>
2020-07-08 20:21:16 +0300

    Initial Commit

---
+++ TestApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApplication
+//
+//  Created by Магомед Абдуразаков on 08.07.2020.
+//  Copyright © 2020 Магомед Абдуразаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
